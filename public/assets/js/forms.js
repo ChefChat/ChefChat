9bd218786a902be080f94f2920c66d83a162d291
James Waite <James Waite>
2017-11-09 11:49:54 -0500

    incorporated cleave.js

---
+++ public/assets/js/forms.js
@@ -0,0 +1,31 @@
+var Cleave = require('cleave.js');
+
+new Cleave('.input-0', {
+	
+});
+
+new Cleave('.input-1', {
+
+});
+
+new Cleave('.input-2', {
+	uppercase: true,
+	  delimiters: [':', ' '],
+	  blocks: [2, 2, 2]
+});
+
+new Cleave('.input-3', {
+	
+});
+
+new Cleave('.input-4', {
+	
+});
+
+new Cleave('.input-5', {
+
+});
+
+new Cleave('.input-6', {
+
+});
